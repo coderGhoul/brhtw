4e8cbfe398a377e9726a329a95b4b18f5616404e
coderGhoul <1773110186@qq.com>
2023-01-06 10:13:15 +0800

    feat: rollup.config.js

--- scripts/rollup/react.config.js
+++ scripts/rollup/react.config.js
@@ -1,42 +1,27 @@
 import { getPackageJSON, resolvePkgPath, getBaseRollupPlugins } from './utils';
 import generatePackageJson from 'rollup-plugin-generate-package-json';
 
-const { name } = getPackageJSON('react');
+//  这里因为 我的pakcage.json 的name字段正好与我的包名一致所以读取name字段
+// model 字段 为 index.ts
+const { name, module } = getPackageJSON('react');
+
+// react的包的路径（源码 packages）
 const pkgPath = resolvePkgPath(name);
+// 打包产物的路径
 const pkgDistPath = resolvePkgPath(name, true);
 
 const basePlugins = getBaseRollupPlugins();
 
+// 这里是rollup打包的基本配置 如果不太明白的可以先看一下rollup的文档（如果后续我学明白了rollup 也会写一些最基本的rollup相关的源码分析）
 export default [
   {
-    input: `${pkgPath}/index.ts`,
+    input: `${pkgPath}/${module}`,
     output: {
       file: `${pkgDistPath}/index.js`,
       name: 'index.js',
-      //  usd 是esm 和commonjs都支持
+      //  usd 能兼容esm 和commonjs都支持
       format: 'umd',
-      plugins: [
-        ...basePlugins,
-        generatePackageJson({
-          inputFolder: pkgPath,
-          outputFolder: pkgDistPath,
-          baseContents: ({ name, description, version }) => ({
-            name,
-            description,
-            version,
-            main: 'index.js',
-          }),
-        }),
-      ],
-    },
-  },
-  {
-    input: `${pkgPath}/src/index.ts`,
-    output: {
-      file: `${pkgDistPath}/index.js`,
-      name: 'index.js',
-      //  usd 是esm 和commonjs都支持
-      format: 'umd',
+      plugins: basePlugins,
     },
   },
 ];
--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -3,15 +3,15 @@ import path from 'path';
 import fs from 'fs';
 
 import ts from 'rollup-plugin-typescript2';
-import replace from '@rollup/plugin-replace';
 import cjs from '@rollup/plugin-commonjs';
 
+//默认的源码路径
 const pkgPath = path.resolve(__dirname, '../../packages');
-// 因为看源码打包的 类似于 
-const distPath = path.resolve(__dirname, '../../dist/node_modules');
+// 因为看源码打包的 类似于  react 、 reactDom 这种包 会打包放在build/node_moudle 里面
+const distPath = path.resolve(__dirname, '../../build/node_modules');
 
 // 解析 路径
-// 举个栗子 ： /packages/react  /dist/node+moudles/react
+// 举个栗子 ： /packages/react  /buld/node+moudles/react
 //因为路径分两种一种是打包的路径一种是源码的路径
 export function resolvePkgPath(packageName, isDist) {
   if (isDist) {
@@ -22,16 +22,21 @@ export function resolvePkgPath(packageName, isDist) {
 
 //接卸package.json  /packages/react 获取到了他pakcage.json => {name:"react"...}
 export function getPackageJSON(pkgName) {
+  //找到package.json
   const path = `${resolvePkgPath(pkgName)}/package.json`;
+  // 使用fs模块读取文件。因为这个地方读取的是字符串
   const str = fs.readFileSync(path, { encoding: 'utf-8' });
+  // 解析字符串
   return JSON.parse(str);
 }
-export function getBaseRollupPlugins({
-  alias = {
-    __LOG__: false,
-    preventAssignment: true,
-  },
-  typescript = {},
-} = {}) {
-  return [replace(alias), cjs(), ts(typescript)];
+
+// 获取所有的基础的plugin 解析成commonjs 和 将ts->js 的plugin
+/**
+ * 1. ts->js rollup-plugin-typescript2 
+ * 2. commonjs @rollup/plugin-commonjs
+ *
+ * install 这两个包
+ * **/
+export function getBaseRollupPlugins({ typescript = {} } = {}) {
+  return [cjs(), ts(typescript)];
 }
